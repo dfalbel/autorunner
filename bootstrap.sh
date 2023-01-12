da0c41f89b2a5edb6bcaf05bb9970a2daec1c11c
Daniel Falbel <dfalbel@gmail.com>
2023-01-12 16:36:08 +0100

    change docker installation steps + add nvidia-docker installation.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,19 +5,8 @@ cd /home/actions
 
 # install docker
 function install_docker () {
-  sudo apt-get update
-  sudo apt-get install \
-      ca-certificates \
-      curl \
-      gnupg \
-      lsb-release
-  sudo mkdir -p /etc/apt/keyrings
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-  echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  sudo apt-get update
-  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+  curl -fsSL https://get.docker.com -o get-docker.sh
+  sudo sh get-docker.sh
 }
 
 install_docker
@@ -28,6 +17,16 @@ then
   # https://cloud.google.com/compute/docs/gpus/install-drivers-gpu
   curl https://raw.githubusercontent.com/GoogleCloudPlatform/compute-gpu-installation/main/linux/install_gpu_driver.py --output install_gpu_driver.py
   sudo python3 install_gpu_driver.py
+
+
+  distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+      && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+      && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | \
+            sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+            sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+  sudo apt-get update
+  sudo apt-get install -y nvidia-docker2
+  sudo systemctl restart docker
 fi
 
 sudo -u actions mkdir actions-runner && cd actions-runner
