822df0a14c729971618c74cb6135420f76f6bcb4
Daniel Falbel <dfalbel@gmail.com>
2023-01-16 15:44:57 +0100

    add shutdown timer for timeout.

--- bootstrap-macos.sh
+++ bootstrap-macos.sh
@@ -3,5 +3,6 @@ curl -o actions-runner-osx-arm64-2.300.2.tar.gz -L https://github.com/actions/ru
 echo "c52f30610674acd0ea7c2d05e65c04c1dedf1606c2f00ce347640a001bafc568  actions-runner-osx-arm64-2.300.2.tar.gz" | shasum -a 256 -c
 tar xzf ./actions-runner-osx-arm64-2.300.2.tar.gz
 ./config.sh --url https://github.com/{org} --token {runner_token} --name {name} --ephemeral --labels {labels} --unattended
+sudo shutdown -h +60
 ./run.sh
-
+sudo shutdown -h +0
