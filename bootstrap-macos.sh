72c1605f95db3479622f2decb489943a722bd415
Daniel Falbel <dfalbel@gmail.com>
2023-01-16 10:00:45 +0100

    use run.sh instead of launching a service.

--- bootstrap-macos.sh
+++ bootstrap-macos.sh
@@ -3,5 +3,5 @@ curl -o actions-runner-osx-arm64-2.300.2.tar.gz -L https://github.com/actions/ru
 echo "c52f30610674acd0ea7c2d05e65c04c1dedf1606c2f00ce347640a001bafc568  actions-runner-osx-arm64-2.300.2.tar.gz" | shasum -a 256 -c
 tar xzf ./actions-runner-osx-arm64-2.300.2.tar.gz
 ./config.sh --url https://github.com/{org} --token {runner_token} --ephemeral --labels {labels} --unattended
-./svc.sh install
-./svc.sh start
+./run.sh
+
