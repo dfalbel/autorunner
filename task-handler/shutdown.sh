30ed6569b44a58b45aae984378c3f78a5466a424
Daniel Falbel <dfalbel@gmail.com>
2023-03-28 10:00:27 +0200

    Fix shutdown script.

--- task-handler/shutdown.sh
+++ task-handler/shutdown.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#! /bin/bash
 
 PREEMPTED=$(curl "http://metadata.google.internal/computeMetadata/v1/instance/preempted" -H "Metadata-Flavor: Google")
 
@@ -6,6 +6,7 @@ echo "Preempred: $PREEMPTED"
 
 # if the VM is shutdown because it was preempted
 if [ "$PREEMPTED" == "TRUE" ]
+then
 # if the machine is turned off, we send a request to the API with the information
 # necessary to re-create the VM
 REQ=$(curl -X POST "https://autorunner-task-scheduler-fzwjxdcwoq-uc.a.run.app/preemptible?instance_id=<instance_id>&labels=<labels>&gpu=<gpu>" -H "accept: */*" -d "")
