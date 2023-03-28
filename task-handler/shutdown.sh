2e7179dc075e204bf97bdb4f7de74fe0a0dd069e
Daniel Falbel <dfalbel@gmail.com>
2023-03-28 09:30:23 +0200

    Remove dead code.

--- task-handler/shutdown.sh
+++ task-handler/shutdown.sh
@@ -9,8 +9,5 @@ if [ "$PREEMPTED" == "TRUE" ]
 # if the machine is turned off, we send a request to the API with the information
 # necessary to re-create the VM
 REQ=$(curl -X POST "https://autorunner-task-scheduler-fzwjxdcwoq-uc.a.run.app/preemptible?instance_id=<instance_id>&labels=<labels>&gpu=<gpu>" -H "accept: */*" -d "")
-REQ=$(curl -X POST "https://autorunner-task-scheduler-fzwjxdcwoq-uc.a.run.app/preemptible" \
-     -H 'Content-Type: application/json' \
-     -d '{"instance_id":"ghgce-12311413742-4534566942-egjlzaeedi","labels": "gce,gpu", gpu: "1"}')
 echo $REQ
 fi
