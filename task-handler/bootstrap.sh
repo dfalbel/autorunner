dd01ebbf4c7d43377ee636c579375c10dcdbfa63
Daniel Falbel <dfalbel@gmail.com>
2023-04-27 10:45:49 +0200

    Update for new installation instructions for contianer toolkit.

--- task-handler/bootstrap.sh
+++ task-handler/bootstrap.sh
@@ -19,14 +19,14 @@ then
   curl https://autorunner-task-handler-fzwjxdcwoq-uc.a.run.app/driver/install_gpu_driver.py --output install_gpu_driver.py
   sudo python3 install_gpu_driver.py
 
-
-  distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+   distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
       && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
-      && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | \
-            sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
-            sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+      && curl -s -L https://nvidia.github.io/libnvidia-container/experimental/$distribution/libnvidia-container.list | \
+         sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+         sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
   sudo apt-get update
-  sudo apt-get install -y nvidia-docker2
+  sudo apt-get install -y nvidia-container-toolkit
+  sudo nvidia-ctk runtime configure --runtime=docker
   sudo systemctl restart docker
 fi
 
