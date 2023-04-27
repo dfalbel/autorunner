e4f9513cfcdff91b2ce74400ec843dee477e00b1
Daniel Falbel <dfalbel@gmail.com>
2023-04-27 12:56:49 +0200

    Do not duplicate delete tasks.

--- task-handler/shutdown.sh
+++ task-handler/shutdown.sh
@@ -9,7 +9,7 @@ DIR="/home/actions/actions-runner/"
 # if the VM is shutdown because it was preempted
 if [ -d "$DIR" ]
 then
-REQ=$(curl -X POST "https://autorunner-task-scheduler-fzwjxdcwoq-uc.a.run.app/preemptible?instance_id=<instance_id>&labels=<labels>&gpu=<gpu>&actions=1" -H "accept: */*" -d "")
+#REQ=$(curl -X POST "https://autorunner-task-scheduler-fzwjxdcwoq-uc.a.run.app/preemptible?instance_id=<instance_id>&labels=<labels>&gpu=<gpu>&actions=1" -H "accept: */*" -d "")
 else
 REQ=$(curl -X POST "https://autorunner-task-scheduler-fzwjxdcwoq-uc.a.run.app/preemptible?instance_id=<instance_id>&labels=<labels>&gpu=<gpu>&actions=0" -H "accept: */*" -d "")
 fi
