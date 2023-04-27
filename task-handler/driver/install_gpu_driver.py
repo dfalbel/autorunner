4a72047221168def25ec7c4805b50b5c943e5000
Daniel Falbel <dfalbel@gmail.com>
2023-04-27 18:17:17 +0200

    Fix driver installation.

--- task-handler/driver/install_gpu_driver.py
+++ task-handler/driver/install_gpu_driver.py
@@ -25,7 +25,6 @@ from typing import Optional
 
 DRIVER_URL = "https://us.download.nvidia.com/tesla/525.105.17/NVIDIA-Linux-x86_64-525.105.17.run"
 K80_DRIVER_URL = "https://us.download.nvidia.com/tesla/470.103.01/NVIDIA-Linux-x86_64-470.103.01.run"
-
 TESLA_K80_DEVICE_CODE = "10de:102d"
 
 class System(Enum):
@@ -345,7 +344,7 @@ def install_driver_runfile(system: System, version: str):
         binary = "NVIDIA-Linux-x86_64-470.103.01.run"
     else:
         run(f"curl -fSsl -O {DRIVER_URL}")
-        binary = "NVIDIA-Linux-x86_64-525.60.13.run"
+        binary = "NVIDIA-Linux-x86_64-525.105.17.run"
 
 
     attempt = 0
