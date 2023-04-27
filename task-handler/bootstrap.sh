cab14d94d53719840ee31b2eff69a44e3640c122
Daniel Falbel <dfalbel@gmail.com>
2023-04-28 00:15:45 +0200

    upgrade runner version?

--- task-handler/bootstrap.sh
+++ task-handler/bootstrap.sh
@@ -36,9 +36,9 @@ else
 fi
 
 sudo -u actions mkdir actions-runner && cd actions-runner
-sudo -u actions curl -o actions-runner-linux-x64-2.300.2.tar.gz -L https://github.com/actions/runner/releases/download/v2.300.2/actions-runner-linux-x64-2.300.2.tar.gz
-sudo -u actions echo "ed5bf2799c1ef7b2dd607df66e6b676dff8c44fb359c6fedc9ebf7db53339f0c  actions-runner-linux-x64-2.300.2.tar.gz" | shasum -a 256 -c
-sudo -u actions tar xzf ./actions-runner-linux-x64-2.300.2.tar.gz
+sudo -u actions curl -o actions-runner-linux-x64-2.303.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.303.0/actions-runner-linux-x64-2.303.0.tar.gz
+sudo -u actions echo "e4a9fb7269c1a156eb5d5369232d0cd62e06bec2fd2b321600e85ac914a9cc73  actions-runner-linux-x64-2.303.0.tar.gz" | shasum -a 256 -c
+sudo -u actions tar xzf ./actions-runner-linux-x64-2.303.0.tar.gz
 sudo -u actions ./config.sh --url https://github.com/{org} --token {runner_token} --ephemeral --labels {labels} --unattended
 ./svc.sh install
 ./svc.sh start
